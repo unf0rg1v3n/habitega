b1883f93436183eadafdf5fb3190d85fa11572d1
w1th0utyou <nek14@vk.com>
2024-04-21 20:17:14 +0600

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
-import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:hebitega/pages/Tasks_page.dart';
 
-main() => runApp(Habitega());
 
-class Habitega extends StatelessWidget {
-  const Habitega({super.key});
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Color(0xFF363636),
-            appBar: AppBar(
-                title: Image.asset("assets/icons/bar.png", // TODO: do it with StatefulWidget
-                    width: 32, height: 32, color: const Color(0xFFD1D0D0)),
-                backgroundColor: const Color(0xFF424141)),
-            body: Container()));
+      title: 'HabitEga',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Taskspage(),
+    );
   }
 }
+
